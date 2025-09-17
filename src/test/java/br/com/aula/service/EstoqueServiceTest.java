6acad9c4a2b0861c477c4ceb03f4c41d8c5af032
pedilha <pedrowilian13@gmail.com>
2025-09-17 20:10:53 -0300

    feat: Implementa listagem de todos os produtos (refs #2)

--- src/test/java/br/com/aula/service/EstoqueServiceTest.java
+++ src/test/java/br/com/aula/service/EstoqueServiceTest.java
@@ -3,6 +3,9 @@ package br.com.aula.service;
 import br.com.aula.model.Produto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*; // Apenas esta linha para asserções
 
 public class EstoqueServiceTest {
@@ -41,5 +44,28 @@ public class EstoqueServiceTest {
         });
 
         assertEquals("A quantidade inicial não pode ser negativa.", exception.getMessage());
+    }
+    @Test
+    void deveListarProdutosCadastrados() {
+        // Preparação
+        estoqueService.cadastrarNovoProduto("Caneta Azul", "Caneta esferográfica", 100);
+        estoqueService.cadastrarNovoProduto("Lápis Preto", "Lápis HB", 200);
+
+        // Ação
+        List<Produto> produtos = estoqueService.listarTodosProdutos();
+
+        // Verificação
+        assertNotNull(produtos);
+        assertEquals(2, produtos.size());
+    }
+
+    @Test
+    void deveRetornarListaVaziaQuandoNaoHouverProdutos() {
+        // Ação
+        List<Produto> produtos = estoqueService.listarTodosProdutos();
+
+        // Verificação
+        assertNotNull(produtos);
+        assertTrue(produtos.isEmpty());
     }
 }
